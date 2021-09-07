886cd1eb94b994c17954e9372a1ac7721d3b2ad7
Justus Schock <12886177+justusschock@users.noreply.github.com>
2021-09-07 19:36:07 +0200

    Update early_stopping.py

--- pytorch_lightning/callbacks/early_stopping.py
+++ pytorch_lightning/callbacks/early_stopping.py
@@ -211,7 +211,7 @@ class EarlyStopping(Callback):
         if reason and self.verbose:
             self._log_info(trainer, reason)
 
-    def _evalute_stopping_criteria(self, current: torch.Tensor) -> Tuple[bool, str]:
+    def _evaluate_stopping_criteria(self, current: torch.Tensor) -> Tuple[bool, str]:
         should_stop = False
         reason = None
         if self.check_finite and not torch.isfinite(current):
